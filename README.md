ce70553061a1fb8b816017d5c104aa4f799e459f
terryso <oxtiger@gmail.com>
2023-12-14 12:51:26 +0800

    完成第一版开发

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# web3_scripts
+## 这是一个批量打CFX铭文的脚本
+> 合约地址： https://evm.confluxscan.net/address/0xc6e865c213c89ca42a622c5572d19f00d84d7a16
+> 
+
+## 如何使用
+1. 要求有node环境
+2. 克隆代码
+    ```
+    https://github.com/terryso/web3_scripts.git
+    ```
+3. 进入代码目录，运行
+    ```
+    npm install ethers
+    ```
+4. 配置钱包私钥，将wallets.json.example改名为wallets.json
+5. 用编辑工具打开wallets.json，将你的私钥填写进去.
+    ```json
+    [
+      {
+        "privateKey": "your private key 1",
+        "transferTimes": 2
+      },
+      {
+        "privateKey": "your private key 2",
+        "transferTimes": 3
+      }
+    ]
+    ```
+    其中privateKey代表一个钱包的私钥，transferTimes表示这个钱包你要转账多少次
+6. 运行脚本
+    ```
+    node cfx_transfer.js
+    ```
---
+++ cfx_transfer.js
@@ -0,0 +1,69 @@
+const { ethers, JsonRpcProvider } = require('ethers');
+const provider = new JsonRpcProvider('https://evm.confluxrpc.com');
+const provider = new JsonRpcProvider('https://cfx-espace.unifra.io/v1/a3f6ab801aaf4ae487f7e78bc224f942');
+const walletsConfig = require('./wallets.json');
+
+// 合约地址
+const receiverAddress = '0xc6e865c213c89ca42a622c5572d19f00d84d7a16';
+
+const wallets = walletsConfig.map(config => ({
+  wallet: new ethers.Wallet(config.privateKey, provider),
+  transferTimes: config.transferTimes
+}));
+
+async function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function transferCfxMultiple(wallets) {
+  const promises = wallets.map(({ wallet, transferTimes }) =>
+    (async () => {
+      for (let i = 0; i < transferTimes; i++) {
+        try {
+          const nonce = await provider.getTransactionCount(wallet.address);
+          const tx = {
+            to: receiverAddress,
+            value: 0,
+            nonce: nonce
+          };
+
+          const txResponse = await wallet.sendTransaction(tx);
+          console.log(`Wallet ${wallet.address} Transaction ${i + 1} hash: ${txResponse.hash}`);
+          await delay(2500);
+
+          let receipt;
+          let retries = 5;
+          while (retries--) {
+            try {
+              receipt = await txResponse.wait();
+              console.log(`Wallet ${wallet.address} Transaction ${i + 1} was confirmed in block ${receipt.blockNumber}`);
+              await delay(2500);
+              break;
+            } catch (error) {
+              if (retries === 0) throw error;
+              console.error(`Error with transaction ${i + 1} from wallet ${wallet.address}, retrying...`, error);
+              await delay(2500); // 添加延迟
+            }
+          }
+        } catch (error) {
+          console.error(`Error with transaction ${i + 1} from wallet ${wallet.address}:`, error);
+        }
+      }
+    })().catch(error => console.error(`Error in wallet ${wallet.address}:`, error))
+  );
+
+  await Promise.all(promises);
+}
+
+async function run() {
+  while (true) {
+    try {
+      await transferCfxMultiple(wallets);
+    } catch (error) {
+      console.error('Error in transferCfxMultiple:', error);
+    }
+    await delay(5000); // 等待一段时间再次运行，以避免过于频繁的请求
+  }
+}
+
+run().catch(console.error);
